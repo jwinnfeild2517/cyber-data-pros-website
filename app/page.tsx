997b33047fa671e5252f214c11164ea63eb08d36
Jenner Thomas <jennerthomas@Jenners-Air.attlocal.net>
2024-01-19 21:10:48 -0500

    fixup

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,7 @@
-import dynamic from 'next/dynamic'
-import PageWrapper from '@/components/PageWrapper'
+import React from 'react'
 
-// const FeedbackCarousel = dynamic(
-//   () => import('@/components/dashboard/FeedbackCarousel'),
-//   { ssr: false },
-// )
-// const AboutSection = dynamic(
-//   () => import('@/components/dashboard/AboutSection'),
-//   {
-//     ssr: false,
-//   },
-// )
-// const ComplianceCarorusel = dynamic(
-//   () => import('@/components/dashboard/ComplianceCarorusel'),
-//   { ssr: false },
-// )
-// const ServiceListDashBoard = dynamic(
-//   () => import('@/components/dashboard/ServiceListDashBoard'),
-//   { ssr: true },
-// )
-
-// const FaqContainer = dynamic(
-//   () => import('@/components/dashboard/FaqContainer'),
-//   { ssr: false },
-// )
-
-const JumbotronContainer = dynamic(() => import('@/components/Jumbotron'), {
-  ssr: true,
-})
-
-export default function Home() {
-  return (
-    <PageWrapper>
-      <JumbotronContainer />
-      {/* <ServiceListDashBoard />
-      <ComplianceCarorusel />
-      <AboutSection />
-      <FeedbackCarousel />
-      <FaqContainer /> */}
-    </PageWrapper>
-  )
+const page = () => {
+  return <div>page</div>
 }
+
+export default page
